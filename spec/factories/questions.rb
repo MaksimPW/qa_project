2ef8165bbe8d461472c54ccf4207f1ab2d387b35
MaksimPW <ma.savichev@gmail.com>
2016-06-30 11:27:10 +0600

    T-2 | Create Questions#create

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     title "MyQuestionMinimumLength"
     body "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
   end
+
+  factory :invalid_question, class: "Question" do
+    title "Invalid"
+    body "Invalid"
+  end
 end
