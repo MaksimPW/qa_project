81eb28fdb8a5cbfafd606840dbb9d197af50fabd
MaksimPW <ma.savichev@gmail.com>
2016-06-30 11:02:47 +0600

    T-2 | Refactor question_controller_spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
-    before do
-      get :index
-    end
+    before { get :index }
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -25,6 +23,7 @@ RSpec.describe QuestionsController, type: :controller do
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
     end
+
     it 'renders show view' do
       expect(response).to render_template :show
     end
