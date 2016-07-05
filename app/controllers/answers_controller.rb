e80bf53fcd1b44fc47c709628120361453299968
MaksimPW <ma.savichev@gmail.com>
2016-07-05 13:59:37 +0600

    T-3 | Only auth user can creates answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!, only: :create
   before_action :load_question
 
   def create
--- spec/features/user_create_answer_to_question_spec.rb
+++ spec/features/user_create_answer_to_question_spec.rb
@@ -1,29 +1,42 @@
 require 'rails_helper'
 
-feature 'User can create answer' do
-  let(:answer) { FactoryGirl.build(:answer) }
-  let(:invalid_answer) { FactoryGirl.build(:invalid_answer) }
-
+feature 'User create answer' do
+  given(:answer) { FactoryGirl.build(:answer) }
+  given(:invalid_answer) { FactoryGirl.build(:invalid_answer) }
+  given(:user) { FactoryGirl.build(:user) }
   given(:question) { create(:question) }
 
-  scenario 'with valid attributes' do
-    visit question_path(question)
+  context 'Authenticated user create answer' do
+    before do
+      User.create!(email: user.email, password: user.password)
+      visit new_user_session_path
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_button 'Log in'
+      visit question_path(question)
+    end
 
-    fill_in 'answer_body', with: answer.body
-    click_button 'Create'
+    scenario 'with valid attributes' do
+      fill_in 'answer_body', with: answer.body
+      click_button 'Create'
 
+      expect(current_path).to eq question_path(question)
+      expect(page).to have_content I18n.t('answers.create.success')
+      expect(page).to have_content answer.body
+    end
 
-    expect(current_path).to eq question_path(question)
-    expect(page).to have_content I18n.t('answers.create.success')
-    expect(page).to have_content answer.body
+    scenario 'with invalid attributes' do
+      fill_in 'answer_body', with: invalid_answer.body
+      click_button 'Create'
+
+      expect(page).to have_content I18n.t('answers.create.fail')
+    end
   end
 
-  scenario 'with invalid attributes' do
+  scenario 'Non-authenticated user create answer' do
     visit question_path(question)
 
-    fill_in 'answer_body', with: invalid_answer.body
-    click_button 'Create'
-
-    expect(page).to have_content I18n.t('answers.create.fail')
+    expect(page).to_not have_content I18n.t('questions.show.new_answer')
+    expect(page).to_not have_button 'Create Answer'
   end
 end
