11ec07a8d6b00a6ab4236b7e9005dd4b2d5ab338
MaksimPW <ma.savichev@gmail.com>
2016-07-05 14:26:09 +0600

    T-3 | Update controllers specs for Auth User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,16 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    'user#{n}@example.com'
+  end
   factory :user do
-    email 'good_user@example.com'
+    email
     password '12345678'
+    password_confirmation '12345678'
   end
 
   factory :invalid_user, class: "User" do
     email 'bad_user@example.com'
     password '1234'
+    password_confirmation '1234'
   end
 end
