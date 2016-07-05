6697669d8dad54b699bbfd0641e61cedc0fecf92
MaksimPW <ma.savichev@gmail.com>
2016-07-05 11:08:37 +0600

    T-3 | User can view list answers in question Show

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :answer do
-    body "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
+    body "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and"
   end
   factory :invalid_answer, class: "Answer" do
     body "Invalid"
