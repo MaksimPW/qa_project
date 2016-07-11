83e534f876c9bcd967914db724f7778316054770
MaksimPW <ma.savichev@gmail.com>
2016-07-12 01:00:09 +0600

    T-4 | Update specs for devise updates

--- spec/features/devise/sign_in_spec.rb
+++ spec/features/devise/sign_in_spec.rb
@@ -7,14 +7,14 @@ feature 'User sign in' do
   scenario 'Registered user try to sign in' do
     sign_in(user)
 
-    expect(page).to have_content 'Signed in successfully'
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
     expect(current_path).to eq root_path
   end
 
   scenario 'Non-registered user try to sign in' do
     sign_in(invalid_user)
 
-    expect(page).to have_content 'Invalid email or password'
+    expect(page).to have_content I18n.t('devise.failure.invalid', authentication_keys: 'Email')
     expect(current_path).to eq new_user_session_path
   end
 end
