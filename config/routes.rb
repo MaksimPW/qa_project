5b0a2a6d2e18bdcb915a06b8ace4f06389efc91c
MaksimPW <ma.savichev@gmail.com>
2016-08-02 22:07:13 +0700

    T-7 | Add lambda instead of concern standart routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root 'questions#index'
 
-  concern :votable do
-    patch :vote_up
-    patch :vote_down
-    delete :vote_destroy
-  end
-
   resources :questions do
     resources :answers, only: [:create, :destroy, :update] do
       patch :best, on: :member
     end
   end
 
-  #FIXME: Refactor this:
-  patch 'answer_vote_up/:id', to: 'answers#vote_up', as: 'answer_vote_up'
-  patch 'answer_vote_down/:id', to: 'answers#vote_down', as: 'answer_vote_down'
-  delete 'answer_vote_destroy/:id', to: 'answers#vote_destroy', as: 'answer_vote_destroy'
+  votable = proc { |model|
+    patch "#{model}_vote_up/:id", to: "#{model}s#vote_up", as: "#{model}_vote_up"
+    patch "#{model}_vote_down/:id", to: "#{model}s#vote_down", as: "#{model}_vote_down"
+    delete "#{model}_vote_destroy/:id", to: "#{model}s#vote_destroy", as: "#{model}_vote_destroy"
+  }
 
-  patch 'question_vote_up/:id', to: 'questions#vote_up', as: 'question_vote_up'
-  patch 'question_vote_down/:id', to: 'questions#vote_down', as: 'question_vote_down'
-  delete 'question_vote_destroy/:id', to: 'questions#vote_destroy', as: 'question_vote_destroy'
+  votable.call('question')
+  votable.call('answer')
+
 end
