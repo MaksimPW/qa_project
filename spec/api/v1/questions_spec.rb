3ae497e4749ec0bdf3614c888d6bdbbd25ab8615
MaksimPW <ma.savichev@gmail.com>
2016-09-09 16:01:20 +0300

    T-13 | Questions API update with serializer

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -36,6 +36,10 @@ describe 'Questions API' do
         end
       end
 
+      it 'question object contains short_title' do
+        expect(response.body).to be_json_eql(question.title.truncate(10).to_json).at_path("0/short_title")
+      end
+
       context 'answers' do
         it 'included in question object' do
           expect(response.body).to have_json_size(1).at_path("0/answers")
