6af4d006439aa528d00214451a66d4d7ee55ec19
MaksimPW <ma.savichev@gmail.com>
2016-10-02 16:19:38 +0300

    T-16 | model Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,5 @@
 class SearchController < ApplicationController
   def search
-      @results = if params.has_key?(:query)
-                 model.search ThinkingSphinx::Query.escape(params[:query])
-               else
-                 []
-               end
-  end
-
-  private
-
-  def model
-    if params.has_key? 'model'
-      if params[:model] == 'All'
-        ThinkingSphinx
-      else
-        params[:model].constantize
-      end
-    else
-      ThinkingSphinx
-    end
+    @results = Search.search_selection(params[:query], params[:model])
   end
 end
